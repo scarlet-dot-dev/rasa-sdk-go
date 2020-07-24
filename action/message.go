40d6cad3a9212042d24553692e8cd31b06140a31
Eddy <eddy@scarlet.dev>
2020-07-24 02:12:36 +0200

    feature: implement json.Marshaler for action.Message to support **kwargs

--- action/message.go
+++ action/message.go
@@ -4,6 +4,8 @@
 
 package action
 
+import "encoding/json"
+
 // Message TODO
 type Message struct {
 	Text        string                 `json:"text,omitempty"`
@@ -16,10 +18,54 @@ type Message struct {
 	Kwargs      map[string]interface{} `json:"-"` // FIXME implement custom ser/de
 }
 
+// ensure interfaces.
+var _ json.Marshaler = (*Message)(nil)
+
+// TODO(ed): is Unmarshaler needed? it seems Message is a one-way dto.
+// var _ json.Unmarshaler = (*Message)(nil)
+
 // WithKwargs adds the free-form kwargs to the m.Kwargs.
 func (m *Message) WithKwargs(kwargs map[string]interface{}) *Message {
 	for key := range kwargs {
 		m.Kwargs[key] = kwargs[key]
 	}
 	return m
+}
+
+// MarshalJSON implements json.Marshaler.
+func (m *Message) MarshalJSON() (data []byte, err error) {
+	if m == nil {
+		return
+	}
+
+	// standard fields
+	var raw JSONMap
+	if m.Text != "" {
+		raw["text"] = m.Text
+	}
+	if m.Image != "" {
+		raw["image"] = m.Image
+	}
+	if m.JSONMessage != nil {
+		raw["json_message"] = m.JSONMessage
+	}
+	if m.Template != "" {
+		raw["template"] = m.Template
+	}
+	if m.Attachment != "" {
+		raw["attachment"] = m.Attachment
+	}
+	if len(m.Buttons) > 0 {
+		raw["buttons"] = m.Buttons
+	}
+	if len(m.Elements) > 0 {
+		raw["elements"] = m.Elements
+	}
+
+	// copy KWargs
+	for key := range m.Kwargs {
+		raw[key] = m.Kwargs[key]
+	}
+
+	return json.Marshal(raw)
 }
